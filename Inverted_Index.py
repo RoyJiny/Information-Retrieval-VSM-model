484fc51d519b41bc0056cb726f7348a137d26865
Roy Jiny <royjiny@gmail.com>
2021-06-29 18:04:31 +0300

    save and load index

--- Inverted_Index.py
+++ Inverted_Index.py
@@ -2,22 +2,25 @@ import xml.etree.ElementTree as ET
 import os
 import math
 from nltk.corpus import stopwords
+import json
 
+INDEX_FILE = "vsm_inverted_index.json"
 
 class InvertedIndex:
-    def __init__(self,path):
+    def __init__(self,path=''):
         self.corpus_path = path
         self.dictionary = {} # map words to a list of tuples of (document,score)
-        self.documents_count = 1
+        self.documents_count = 0
 
     def build_dictionary(self):
         stop_words = set(stopwords.words("english"))
         
-        self.documents_count = len(os.listdir(self.corpus_path))
+        self.documents_count = 0
         for file in os.listdir(self.corpus_path):
             xml_path = os.path.join(self.corpus_path,file)
             tree = ET.parse(xml_path)
             root = tree.getroot()
+            self.documents_count += len(root.findall("./RECORD"))
             for record in root.findall("./RECORD"):
                 file_id = record.find("./RECORDNUM").text.replace(" ",'')
                 
@@ -29,7 +32,7 @@ class InvertedIndex:
                     text += record.find("./ABSTRACT").text.replace("\n"," ")
                 
                 # convert to tokens
-                tokens = [word.lower() for word in text.split(" ")]
+                tokens = [word.lower() for word in text.split(" ") if word != '']
                 tf_dict = {}
                 
                 # remove stop words and duplications
@@ -49,13 +52,31 @@ class InvertedIndex:
                     else:
                         self.dictionary[token] = [ [file_id,tf_dict[token]] ]
 
-    
     def update_tfidf_scores(self):
         for word in self.dictionary.keys():
             df = len(self.dictionary[word])
             idf_score = math.log2(self.documents_count/df)
             for doc in self.dictionary[word]:
                 doc[1] = doc[1] * idf_score # update from tf score to tf-idf score for each document
+
+    def save(self):
+        data = {
+            "doc count": self.documents_count,
+            "original path": self.corpus_path,
+            "dictionary": self.dictionary
+        }
+        json_object = json.dumps(data, indent=4)
+        with open(INDEX_FILE,'w+') as json_file:
+            json_file.write(json_object)
+
+
+def load_index(path=INDEX_FILE):
+    with open(path) as json_file:
+        data = json.load(json_file)
+    index = InvertedIndex(data["original path"])
+    index.documents_count = data["doc count"]
+    index.dictionary = data["dictionary"]
+    return index
 
 def create_index(path):
     print(f"Creating an inverted index on path {path}")
@@ -64,3 +85,5 @@ def create_index(path):
     index.build_dictionary()
     print("Updating tf-idf scores")
     index.update_tfidf_scores()
+    print(f"Saving index under {INDEX_FILE}")
+    index.save()
